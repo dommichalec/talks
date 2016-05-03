173b682b579e0ab70891564b6e5bf86dd563738d
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-05-03 14:09:29 -0400

    put index page on a grid

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -6,4 +6,23 @@ class TalksController < ApplicationController
   def show
     @talk = Talk.find(params[:id])
   end
+
+  def edit
+    @talk = Talk.find(params[:id])
+  end
+
+  def update
+    @talk = Talk.find(params[:id])
+    if @talk.update(talk_params)
+      redirect_to @talk, notice: "#{@talk.title} has been updated!"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def talk_params
+    params.require(:talk).permit(:title, :topic, :speaker, :speaker_profile, :date)
+  end
 end
