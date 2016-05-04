0fbb47b19982a964aa2e697e9ce3c33b48efa7f5
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-05-04 14:19:24 -0400

    update content in talks helper

--- app/helpers/talks_helper.rb
+++ app/helpers/talks_helper.rb
@@ -1,7 +1,7 @@
 module TalksHelper
   def display_registration_for(talk)
     if talk.sold_out?
-      link_to "Talk is sold out!", '#', class: 'label label-pill label-danger'
+      content_tag(:p, "Talk is sold out!", class: 'label label-pill label-danger')
     else
       link_to "Registration is now open!", new_talk_registration_path(talk), class: 'label label-pill label-success'
     end
