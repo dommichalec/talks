f4b86411abd66a5f7169ab3b4787e3641c2b8ce3
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-05-04 14:10:51 -0400

    refactor talk index view

--- app/helpers/talks_helper.rb
+++ app/helpers/talks_helper.rb
@@ -1,2 +1,9 @@
 module TalksHelper
+  def display_registration_for(talk)
+    if talk.sold_out?
+      link_to "Talk is sold out!", '#', class: 'label label-pill label-danger'
+    else
+      link_to "Registration is now open!", new_talk_registration_path(talk), class: 'label label-pill label-success'
+    end
+  end
 end
