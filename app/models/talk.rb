f587e15a058bf6ec926a5b31e9f155b1c1dc10d3
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-05-04 10:28:07 -0400

    add talk validations and error handling view template

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,5 +1,10 @@
 class Talk < ActiveRecord::Base
 
+  validates :title, :speaker, presence: true
+  validates :topic, length: { minimum: 45 }
+  validates :speaker_profile, length: { minimum: 45 }
+  validates :capacity, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  
   def self.upcoming
     where('date >= ?', Time.now).order('date')
   end
