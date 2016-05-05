5a2eb00525b8ac60ff1e2c826a692f60cbd87e57
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-05-05 19:39:37 -0400

    add perspective helper method to user show page depending on who is viewing it

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,4 +3,12 @@ module UsersHelper
     url = "https://secure.gravatar.com/avatar/#{user.gravatar_id}"
     image_tag(url, alt: user.first_name)
   end
+
+  def get_perspective(user)
+    if user == current_user
+      "you've"
+    else
+      "#{user.first_name} has"
+    end
+  end
 end
