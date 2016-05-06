bd1a75eb8eeee8d249d3d4e1704ad88b763fbf1d
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-05-06 12:06:31 -0400

    update helpful/unhelpful so user can only perform either action once

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,9 +1,21 @@
 class LikesController < ApplicationController
   before_action :require_signin
+  before_action :set_event, only: [:create, :destroy]
 
   def create
-    @talk = Talk.find(params[:talk_id])
     @talk.likes.create!(user: current_user)
     redirect_to @talk, notice: "Glad you found #{@talk.title} helpful!"
+  end
+
+  def destroy
+    like = current_user.likes.find(params[:id])
+    like.destroy
+    redirect_to @talk, notice: "Ah, sorry you didn't find this talk very helpful!"
+  end
+
+  private
+
+  def set_event
+    @talk = Talk.find(params[:talk_id])
   end
 end
